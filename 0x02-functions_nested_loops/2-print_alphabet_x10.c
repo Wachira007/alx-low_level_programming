d8f35aad393c136bfb7a14457ebb34362e2baac7
Wachira007 <dannielmutua00@gmail.com>
2022-06-07 17:33:16 -0700

    Added code to the print_alphabet file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
+
+/**
+ * main - entry point
+ * return 0 - success
+ * */
+
+int main (void)
+{
+	char letter;
+	for letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
