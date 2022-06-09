44a2a38607752bd1c811dcdf6e26de7aaa0ba594
Wachira007 <dannielmutua00@gmail.com>
2022-06-09 06:28:05 -0700

    addded the code that prints a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,16 @@
 #include "main.h"
+/**
+*print_alphabet- prints letters a to z
+*Return:
+*/
 
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
