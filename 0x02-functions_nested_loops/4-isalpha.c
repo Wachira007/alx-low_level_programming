9baec8256f9f361eb2d4989083d9dd41dac9276d
Wachira007 <dannielmutua00@gmail.com>
2022-06-09 09:41:47 -0700

    Prints out the characters are uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,12 @@
 #include "main.h"
+
+/**
+*_isalpha - checks for alphabetic character
+*@c: takes an integer
+*Return: 1(success)
+*/
+
+int _isaplha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
