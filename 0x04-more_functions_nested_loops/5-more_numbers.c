c4b06bf4ae52681a421cf73b51284ed394736e87
Wachira007 <dannielmutua00@gmail.com>
2022-06-09 19:06:03 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+*more_numbers- prints more numbers
+*Return:
+*/
+
+void more_numbers(void)
+{
+	int x, y;
+	for (x = 0; x < 10; x++)
+	{
+		 for (y = 0; y < 15; y++)
+		{
+			_putchar (y + '0');
+		}
+		_putchar('\n');
+
+	}
+}
