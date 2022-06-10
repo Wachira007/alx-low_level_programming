88652670134eb942d3bdb89330a8cdedc1fd5e3b
Wachira007 <dannielmutua00@gmail.com>
2022-06-09 19:28:14 -0700

    Add a character line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_line - print line
+*@n: integer
+*
+*/
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x ++)
+		_putchar('_');
+	_putchar('\n');
+}
