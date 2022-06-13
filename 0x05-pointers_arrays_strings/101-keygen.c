76aec530793cb717d707be4eeacd64105fac761c
Wachira007 <dannielmutua00@gmail.com>
2022-06-13 12:28:22 -0700

    hate the code- hacker

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int r = 0, q = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (q < 2772)
+	{
+		r = rand() % 128;
+		if ((q + r) > 2772)
+			break;
+		q = q + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - q));
+	return (0);
+}
