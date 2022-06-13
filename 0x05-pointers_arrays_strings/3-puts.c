2f75dafed47a6810a0157f6e99ffcdee29cf4574
Wachira007 <dannielmutua00@gmail.com>
2022-06-13 12:05:30 -0700

    Prints a string followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,19 @@
 #include "main.h"
+
 /**
-*_puts- prints a string  put command
-*@str:  pointer to an int that will change
+*_puts - prints a string
+*@str: pointer to the string to print
 *Return: void
 */
 
 void _puts(char *str)
 {
-	char *c
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
