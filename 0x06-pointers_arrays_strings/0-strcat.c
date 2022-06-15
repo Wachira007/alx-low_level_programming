3fe09f77581d37cf4b931392018c8a32c300859e
Wachira007 <dannielmutua00@gmail.com>
2022-06-15 06:10:55 -0700

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* *_strcat - commute srtings
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
