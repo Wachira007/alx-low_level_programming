6548f3cfa5300ba91b642b2760b716ec6a311313
Wachira007 <dannielmutua00@gmail.com>
2022-06-15 10:29:21 -0700

    error corrected

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,41 +11,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, y, x, dr1, dr2, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	if (i >= j)
-		x = i;
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		x = j;
+		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
-	r[x + 1] = '\0';
-	i--, j--, size_r--;
-	dr1 = *(n1 + i) - 48, dr2 = *(n2 + j) - 48;
-	while (x >= 0)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		y = dr1 + dr2 + add;
-		if (y >= 10)
-			add = y / 10;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
 		else
 			add = 0;
-		if (y > 0)
-		*(r + x) = (y % 10) + 48;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
 		else
-			*(r + x) = '0';
-		if (i > 0)
-			i--, dr1 = *(n1 + i) - 48;
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
-		if (j > 0)
-			j--, dr2 = *(n2 + j) - 48;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
-		x--, size_r--;
+		bg--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
