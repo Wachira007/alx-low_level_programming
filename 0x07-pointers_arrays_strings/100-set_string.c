9e40efe0ad16add88b907c53562890c11fec1d82
Wachira007 <dannielmutua00@gmail.com>
2022-06-20 11:30:56 -0700

    sets the value of a pointer to a character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* set_string - sets pointer to a char
+* @s: source address
+* @to: targeted address
+*Return: 0
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char);
 void print_chessboard(char (*a)[8]);
-
+void set_string(char **s, char *to);
 #endif
