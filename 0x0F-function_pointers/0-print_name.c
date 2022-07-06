dfb6997b510984b2f93996a7d02375aa19ce34fe
Wachira007 <dannielmutua00@gmail.com>
2022-07-06 05:34:24 -0700

    corrected the error on the code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
 
 /**
-*print_name- lets prints a name
-*@name : name input
-*@f: function pointer
-*Return: 0
-*/
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
